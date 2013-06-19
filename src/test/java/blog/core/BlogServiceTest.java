c1d7107e3da3e15e5e647fabaae2c5f45e0fb5e2
Christian Baranowski <baranowski@N-Miraculix.seitenbau.net>
2013-06-19 16:42:52 +0200

    add test for Blog Service.

---
+++ src/test/java/blog/core/BlogServiceTest.java
@@ -0,0 +1,40 @@
+package blog.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BlogServiceTest
+{
+
+  @Test
+  public void testSave()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindTags()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindAll()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testFindById()
+  {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testDeleteById()
+  {
+    fail("Not yet implemented");
+  }
+
+}
