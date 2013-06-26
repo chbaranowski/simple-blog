0eb79eed4fd1dbdb06a5130da5441add064c3a48
Christian Baranowski <baranowski@N-Miraculix.seitenbau.net>
2013-06-26 18:47:09 +0200

    fix setup script

--- src/main/resources/db/migration/V3__Add_Tag_Table.sql
+++ src/main/resources/db/migration/V3__Add_Tag_Table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Post_Tag (
-  post_id int NOT NULL,
+  posts_id int NOT NULL,
   tags_name varchar(255) NOT NULL,
   KEY FK_TAGS_NAME_KEY (tags_name),
-  KEY FK_POST_ID (post_id)
+  KEY FK_POST_ID (posts_id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE Tag (
