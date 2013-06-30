f70db2b8a87fb6b4d831cf5c8afccfd1c5f8f4f3
Christian Baranowski <develop@pro-pix.de>
2013-06-30 21:16:14 +0200

    add some unit tests for the domain class basar service

--- src/main/java/blog/core/Tag.java
+++ src/main/java/blog/core/Tag.java
@@ -18,6 +18,12 @@ public class Tag implements Serializable {
     
     @ManyToMany(mappedBy="tags")
     private List<Post> posts = new ArrayList<Post>();
+    
+    public Tag() {}
+
+    public Tag(String name) {
+        this.name = name;
+    }
 
     public String getName() {
         return name;
@@ -33,6 +39,31 @@ public class Tag implements Serializable {
 
     public void setPosts(List<Post> posts) {
         this.posts = posts;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tag other = (Tag) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
     }
     
 }
--- src/test/java/blog/core/BlogServiceTest.java
+++ src/test/java/blog/core/BlogServiceTest.java
@@ -1,7 +1,10 @@
 package blog.core;
 
-import static org.junit.Assert.*;
+import static org.fest.assertions.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +12,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 public class BlogServiceTest
 {
@@ -35,12 +40,79 @@ public class BlogServiceTest
     post.setContent("Content");
     Tags tags = new Tags();
     tags.setNames("OSGi");
+    
+    when(tagRepository.save(any(Tag.class))).thenAnswer(new Answer<Tag>() {
+        @Override
+        public Tag answer(InvocationOnMock invocation) throws Throwable {
+            return (Tag) invocation.getArguments()[0];
+        }
+    });
+    
     blogService.save(post , tags);
     
     ArgumentCaptor<Post> postCaptor = ArgumentCaptor.forClass(Post.class);
     verify(postRepository).save(postCaptor.capture());
     Post savedPost = postCaptor.getValue();
-    assertEquals("Title", savedPost.getTitle());
+    assertThat(savedPost.getTitle()).isEqualTo("Title");
+    assertThat(savedPost.getContent()).isEqualTo("Content");
+    assertThat(savedPost.getTags()).containsExactly(new Tag("OSGi"));
+  }
+  
+  @Test
+  public void save_withExistingTag()
+  {
+    Post post = new Post();
+    post.setTitle("Title");
+    post.setContent("Content");
+    Tags tags = new Tags();
+    tags.setNames("OSGi");
+    Tag osgiTag = new Tag();
+    osgiTag.setName("OSGi Test Tag");
+    when(tagRepository.findOne("OSGi")).thenReturn(osgiTag);
+    blogService.save(post , tags);
+    
+    ArgumentCaptor<Post> postCaptor = ArgumentCaptor.forClass(Post.class);
+    verify(postRepository).save(postCaptor.capture());
+    Post savedPost = postCaptor.getValue();
+    assertThat(savedPost.getTags()).containsExactly(new Tag("OSGi Test Tag"));
+  }
+  
+  @Test
+  public void findTags() throws Exception {
+    Post post = new Post();
+    post.getTags().add(new Tag("OSGi"));
+    post.getTags().add(new Tag("Java"));
+    Tags tags = blogService.findTags(post);
+    assertThat(tags.getNames()).isEqualTo("OSGi,Java");
+  }
+  
+  @Test
+  public void findTags_NoTags() throws Exception {
+    Post post = new Post();
+    Tags tags = blogService.findTags(post);
+    assertThat(tags.getNames()).isEqualTo("");
+  }
+  
+  @Test
+  public void findAll() throws Exception {
+    List<Post> posts = new ArrayList<Post>();
+    when(postRepository.findAll()).thenReturn(posts);
+    Iterable<Post> actualPosts = blogService.findAll();
+    assertThat((List<Post>) actualPosts).isEqualTo(posts);
+  }
+  
+  @Test
+  public void findById() throws Exception {
+    Post post = new Post();
+    when(postRepository.findOne(42L)).thenReturn(post);
+    Post actualPost = blogService.findById(42L);
+    assertThat(actualPost).isEqualTo(post);
+  }
+  
+  @Test
+  public void deleteById() throws Exception {
+    blogService.deleteById(42L);
+    verify(postRepository).delete(42L);
   }
 
 }
