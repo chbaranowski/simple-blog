55ddc0ee33b4ad02581044cc755349c3fb41d1a3
Christian Baranowski <develop@pro-pix.de>
2013-06-30 21:52:57 +0200

    add some auth controller tests

--- src/test/java/blog/web/rest/AuthResourceControllerTest.java
+++ src/test/java/blog/web/rest/AuthResourceControllerTest.java
@@ -7,6 +7,8 @@ import static org.mockito.Mockito.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -30,8 +32,26 @@ public class AuthResourceControllerTest {
     }
     
     @Test
-    public void auth() {
-        
+    public void auth() throws Exception {
+        AuthToken token = new AuthToken("DummyAuthToken");
+        when(authService.auth("usr", "pwd")).thenReturn(token);
+        String json = mvc.perform(post("/rest/auth")
+                        .header("authname", "usr")
+                        .header("authpass", "pwd")
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andReturn().getResponse().getContentAsString();
+        assertThat(json).contains("DummyAuthToken");
+    }
+    
+    @Test
+    public void auth_unknownUser() throws Exception {
+        when(authService.auth("usr", "pwd")).thenThrow(new AuthException());
+        int status = mvc.perform(post("/rest/auth")
+                .header("authname", "usr")
+                .header("authpass", "pwd")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse().getStatus();
+        assertThat(status).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
 }
